7df7ed576d348555d571b28dfd2f868a9c62efa1
Antoine Lebègue <antoinelebegue@outlook.com>
2026-06-12 00:37:39 +0200

    Patched image loading (broken, only 1 game works for some reasons)

--- Plugin.cs
+++ Plugin.cs
@@ -1,5 +1,9 @@
-﻿using BepInEx;
+﻿using System.Reflection;
+using BepInEx;
 using BepInEx.Logging;
+using HarmonyLib;
+using SteamShelf;
+using UnityEngine;
 
 namespace AutoPull;
 
@@ -12,6 +16,50 @@ public class Plugin : BaseUnityPlugin
     {
         // Plugin startup logic
         Logger = base.Logger;
-        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID} is loaded!");
+        
+        Harmony harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+        harmony.PatchAll(typeof(FetchPatch));
+    }
+
+    [HarmonyPatch]
+    class FetchPatch
+    {
+        // can't access it directly (class is internal)
+        [HarmonyTargetMethod]
+        static MethodBase TargetMethod()
+        {
+            var type = AccessTools.TypeByName("SteamShelf.SteamGameCache");
+            if (type == null) Logger.LogError("Can't access SteamGameCache class!");
+            
+            var method = AccessTools.Method(type, "TryLoadBoxArtAsync");
+            if (method == null) Logger.LogError("Can't access TryLoadBoxArtAsync method!");
+            else Logger.LogInfo("Patched TryLoadBoxArtAsync successfully!");
+            
+            return method;
+        }
+        
+        [HarmonyPrefix]
+        static bool Prefix(SteamGameData data)
+        {
+            var fetcher = AccessTools.StaticFieldRefAccess<object>(typeof(SteamLibrarySystem), "fetcher");
+            if(fetcher == null)
+            {
+                Debug.LogError("Couldn't access fetcher!");
+                return true;
+            }
+            
+            var fetchMethod =
+                AccessTools.Method(AccessTools.TypeByName("SteamShelf.SteamStoreFetcher"), "FetchBoxArtAsync");
+            if (fetchMethod == null)
+            {
+                Debug.LogError("Couldn't access FetchBoxArtAsync method!");
+                return true;
+            }
+
+            var method = MethodInvoker.GetHandler(fetchMethod, true);
+            method.Invoke(fetcher, data);
+            return false;
+        }
     }
 }
